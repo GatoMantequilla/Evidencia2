83a925d5f5cc78a38f371fb96f72860a3557ba43
zerochaos02 <b.martinez12@ufromail.cl>
2024-09-02 16:43:01 -0400

    commit 1

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,5 +1,6 @@
 
 public class HolaMundo {
     public static void main(String[] args) {
-    System.out.printf("Hola Mundo!!!! Daniela Díaz ");
-    System.out.println("Eduardo Huidobro");
+        System.out.println("Hola Mundo");
+    }
+}
