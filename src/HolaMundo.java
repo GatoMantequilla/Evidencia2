6fe2a0af40975d63345d83631161587c48d2668f
ZeroChaos <142537679+zerochaos02@users.noreply.github.com>
2024-09-02 18:56:15 -0400

    Update HolaMundo.java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,10 +1,9 @@
 
 public class HolaMundo {
     public static void main(String[] args) {
-
-    System.out.print("Hola Mundo");
+    System.out.println("Hola Mundo");
     System.out.println("Benjamin Martinez");
-    System.out.print("Hola Mundo!!!! Daniela Díaz ");
+    System.out.println("Daniela Díaz");
     System.out.println("Eduardo Huidobro");
     System.out.println("Camilo Cifuentes");
     }
